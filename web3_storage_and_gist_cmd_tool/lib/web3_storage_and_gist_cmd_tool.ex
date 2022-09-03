a1fd47fb7c7e525bc967386a5573dfa0a4c98e25
leeduckgo <albertschr@163.com>
2022-09-03 22:00:43 +0800

    update with CLI Module

--- web3_storage_and_gist_cmd_tool/lib/web3_storage_and_gist_cmd_tool.ex
+++ web3_storage_and_gist_cmd_tool/lib/web3_storage_and_gist_cmd_tool.ex
@@ -17,13 +17,26 @@ defmodule Web3StorageAndGistCmdTool.CLI do
   end
 
   def handle_args([gist: gist_id, to: "ipfs" = to, updategist: true], []) do
-    {:ok,
+    try do
+      {:ok,
       %{
         "Hash" => hash
     }} =
       gist_to_ipfs(gist_id)
+
     io_puts_upload_info(to, hash)
-    # TODO: update the gist
+    {:ok, _payload} =
+      update_gist(to, gist_id, hash)
+
+    IO.puts "gist #{gist_id} has updated already."
+    rescue
+      error ->
+        IO.puts "meet error: #{inspect(error)}"
+    end
+  end
+
+  def handle_args([gist: gist_id, to: "ipfs" = to, updategist: false], []) do
+    handle_args([gist: gist_id, to: "ipfs" = to], [])
   end
 
   def handle_args([gist: gist_id, to: "ipfs" = to], []) do
@@ -52,16 +65,28 @@ defmodule Web3StorageAndGistCmdTool.CLI do
   end
 
   def handle_args([], ["help"]) do
-    "send gist to IPFS:\n./web3_storage_and_gist_cmd_tool --gist [gist_id] --to ipfs\n"
-    |> Kernel.<>("send gist to ARWEAVE: \n./web3_storage_and_gist_cmd_tool --gist [gist_id] --to ar\n")
+    "send gist to IPFS:\n./web3_storage_and_gist_cmd_tool --gist [gist_id] --to ipfs --updategist [true or false]\n"
+    |> Kernel.<>("send gist to ARWEAVE: \n./web3_storage_and_gist_cmd_tool --gist [gist_id] --to ar --updategist [true or false]\n")
     |> Kernel.<>("generate config:\n ./web3_storage_and_gist_cmd_tool --genconf []")
     |> IO.puts()
   end
 
-  def update_gist_with_ipfs_info(gist_id, hash) do
-    payload = GistHandler.get_gist(gist_id)
+  def update_gist(to, gist_id, hash) do
+    %{files: files} = payload = GistHandler.get_gist(gist_id)
+    [file_name, value] = build_file(to, hash)
+    files_new =
+      files
+      |> Map.put(file_name, value)
+    payload_new =
+      Map.put(payload, :files, files_new)
+    GistHandler.update_gist(gist_id, payload_new)
   end
 
+  def build_file("ipfs", hash) do
+    [String.to_atom("IPFS_RECORD.json"), %{content: Poison.encode!(%{cid: hash})}]
+  end
+
+  @spec io_puts_upload_info(any, any) :: :ok
   def io_puts_upload_info(ar_of_ipfs, hash) do
     IO.puts "the Gist is upload to #{ar_of_ipfs} already!Hash is: #{hash}"
   end
